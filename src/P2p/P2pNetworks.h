6f688c8de66dc62b6dcfec2757ab50d394abcf72
Mark Allen Evans <president@worldvaporexpo.com>
2017-12-02 04:30:22 +0000

    safe

---
+++ src/P2p/P2pNetworks.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2011-2016 The Cryptonote developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#pragma once
+
+namespace CryptoNote
+{                                                           
+  const static boost::uuids::uuid CRYPTONOTE_NETWORK = { { 0x3d, 0x14, 0xe3, 0x4f, 0x49, 0x8, 0x19, 0x2b, 0x31, 0x8, 0x2b, 0x31, 0x8, 0x8, 0x19, 0xe3 } };
+}
